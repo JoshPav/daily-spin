cdc5e72419f7ab0411c8a600cb04a52ee1e8306c
Josh Paveley <32667622+JoshPav@users.noreply.github.com>
2026-01-13 22:46:49 +0000

    Update auth-client.ts

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,7 +1,4 @@
 import { createAuthClient } from 'better-auth/vue';
 
 export const { signIn, signOut, useSession, getSession, getAccessToken } =
-  createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: 'http://127.0.0.1:3000',
-  });
+  createAuthClient();
