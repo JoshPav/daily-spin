07299036d00f2d38857e1b28b042951a6a9ef178
Josh Paveley <joshpaveley@gmail.com>
2026-01-14 21:48:51 +0000

    test: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -63,6 +63,8 @@ export default defineConfig({
           environment: 'node',
           include: ['**/*.integration.ts'],
           exclude: ['node_modules/**'],
+          setupFiles: ['./tests/integration.setup.ts'],
+          fileParallelism: false,
         },
       },
     ],
